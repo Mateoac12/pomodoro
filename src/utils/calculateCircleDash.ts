9fd84d050a97c0b7d1624793f35712a2c2d3c644
Mateo Álvarez <alvarez.fing@gmail.com>
2022-02-10 16:07:53 -0300

    feat: include dynamic control of progress bar

--- src/utils/calculateCircleDash.ts
+++ src/utils/calculateCircleDash.ts
@@ -1,13 +1,17 @@
 interface TimeProps {
   minutes: number
   seconds: number
+  totalSeconds: number
 }
 
-export const calculateCicrleDash = ({ minutes, seconds }: TimeProps) => {
+export const calculateCicrleDash = ({
+  minutes,
+  seconds,
+  totalSeconds,
+}: TimeProps) => {
   const normalizeMinutes = minutes * 60
-
   const totalSecondsLeft = normalizeMinutes + seconds
-  const totalSecondsTracked = 1500 // TODO: son 25 minutos, luego debo hacerlo dinamico
+  const totalSecondsTracked = totalSeconds
 
   const timeDiferenceFraction = totalSecondsLeft / totalSecondsTracked
   const circleTime =
