26398dcfbc3a0ddfeafb4f0071723ae8bb4bdddb
Mateo Álvarez <alvarez.fing@gmail.com>
2022-02-16 13:37:32 -0300

    feat: add step changes and browser notifications

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,88 +1,27 @@
-import { useCallback, useState } from 'react'
-import { useTimer } from 'react-timer-hook'
-import { getCounterTime } from 'utils/getCounterTime'
-import TimeCounter from './TimeCounter'
-import audio from 'assets/audio/alert-ding.mp3'
-import { playSound } from 'utils/playSound'
-import PlayButton from 'components/PlayButton'
+import { useNotifications } from 'hooks/useNotification'
+
 import PauseButton from 'components/PauseButton'
-
-interface TimerState {
-  initialTime: Date
-  isWorkTime: boolean
-}
-
-const initialTime = getCounterTime(1)
-
-const initialState = {
-  initialTime,
-  isWorkTime: true,
-}
-
-export const useNotifications = () => {
-  const askPermission = useCallback(() => Notification.requestPermission(), [])
-
-  const playNotification = useCallback(
-    (message: string) =>
-      askPermission().then(() => {
-        if (Notification.permission !== 'denied') {
-          return new Notification('Tomatito', {
-            lang: 'es-ES',
-            body: message,
-          })
-        }
-      }),
-    []
-  )
-
-  return {
-    askPermission,
-    playNotification,
-  }
-}
+import PlayButton from 'components/PlayButton'
+import TimeCounter from './TimeCounter'
+import { useTimerControl } from 'hooks/useTimerControl'
 
 const Counter = () => {
   const { askPermission, playNotification } = useNotifications()
-  const [counterTime, setCounterTime] = useState<TimerState>(initialState)
-  const { initialTime, isWorkTime } = counterTime
-
   askPermission()
 
-  const { pause, minutes, seconds, isRunning, resume, restart } = useTimer({
-    autoStart: false,
-    expiryTimestamp: initialTime,
-    onExpire: handleChangeTimer,
-  })
-
-  const pauseCallback = useCallback(() => pause(), [])
-
-  function handleChangeTimer() {
-    const timeOfCounter = getCounterTime(1)
-    setCounterTime((prevState) => ({
-      ...prevState,
-      isWorkTime: !prevState.isWorkTime,
-      initialTime: timeOfCounter,
-    }))
-
-    restart(timeOfCounter)
-    playSound(audio)
-
-    playNotification(
-      isWorkTime
-        ? '¡Tiempo de trabajo finalizado!'
-        : '¡Tiempo de descanso finalizado!'
-    )
-  }
+  const { isRunning, maxTime, minutes, pause, resume, seconds, isWorkTime } =
+    useTimerControl({ playNotification })
 
   return (
     <div className="relative bg-stone-50 border flex flex-col mx-auto my-auto rounded-full shadow-md">
       <TimeCounter
+        maxTime={maxTime}
         minutes={minutes}
         seconds={seconds}
         isWorkTime={isWorkTime}
       />
       {isRunning ? (
-        <PauseButton pause={pauseCallback} />
+        <PauseButton pause={pause} />
       ) : (
         <PlayButton resume={resume} />
       )}
