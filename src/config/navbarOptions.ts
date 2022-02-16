711fdb7307a93e5c8f1f31119be05b1aaebabf8c
Mateo Álvarez <alvarez.fing@gmail.com>
2022-02-16 14:44:32 -0300

    feat: add methodolody info and rules

--- src/config/navbarOptions.ts
+++ src/config/navbarOptions.ts
@@ -4,6 +4,18 @@ export interface NavbarOptions {
   url: string
 }
 
+export interface TrackerOptions {
+  id: number
+  name: string
+  rules: TrackerRules[]
+}
+
+export interface TrackerRules {
+  flip: number
+  time: number
+  isWorkTime: true | false
+}
+
 // is important to have a numeric id because it is used as a delay for the animation
 export const navbarOptions: NavbarOptions[] = [
   {
@@ -20,5 +32,86 @@ export const navbarOptions: NavbarOptions[] = [
     id: 2,
     name: 'Configuración',
     url: '/config',
+  },
+]
+
+export const trackerOptions: TrackerOptions[] = [
+  {
+    id: 0,
+    name: 'Pomodoro',
+    rules: [
+      {
+        flip: 0,
+        time: 25,
+        isWorkTime: true,
+      },
+      {
+        flip: 1,
+        time: 5,
+        isWorkTime: false,
+      },
+      {
+        flip: 2,
+        time: 25,
+        isWorkTime: true,
+      },
+      {
+        flip: 3,
+        time: 5,
+        isWorkTime: false,
+      },
+      {
+        flip: 4,
+        time: 25,
+        isWorkTime: true,
+      },
+      {
+        flip: 5,
+        time: 5,
+        isWorkTime: false,
+      },
+      {
+        flip: 6,
+        time: 25,
+        isWorkTime: true,
+      },
+      {
+        flip: 7,
+        time: 15,
+        isWorkTime: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: 'Pomodoro+',
+    rules: [
+      {
+        flip: 0,
+        time: 45,
+        isWorkTime: true,
+      },
+      {
+        flip: 1,
+        time: 15,
+        isWorkTime: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'FlowTime',
+    rules: [
+      {
+        flip: 0,
+        time: 90,
+        isWorkTime: true,
+      },
+      {
+        flip: 1,
+        time: 10,
+        isWorkTime: false,
+      },
+    ],
   },
 ]
