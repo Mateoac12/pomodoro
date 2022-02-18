c7f44b426c32fdacc28a66a48aeeeb60c2a71ec0
Mateo Álvarez <alvarez.fing@gmail.com>
2022-02-18 20:53:29 -0300

    fix: change p tag to div tag

--- src/components/MethodologyHomeTag/PomodoroCustom.tsx
+++ src/components/MethodologyHomeTag/PomodoroCustom.tsx
@@ -1,10 +1,10 @@
 const FlowTimeDescription = () => {
   return (
-    <p>
+    <div>
       Nuestro Pomodoro consiste en una sesión de{' '}
       <span className="font-bold text-orange-600">90</span> minutos de trabajo y{' '}
       <span className="font-bold text-green-600">10</span> minutos de descanso.
-    </p>
+    </div>
   )
 }
 
