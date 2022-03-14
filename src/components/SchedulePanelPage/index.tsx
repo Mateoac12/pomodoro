1927f15ec8e43c0d8be9e9e0acdc3ec85b3c56dc
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-14 16:49:58 -0300

    feat: apply information of database

--- src/components/SchedulePanelPage/index.tsx
+++ src/components/SchedulePanelPage/index.tsx
@@ -2,15 +2,97 @@ import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import { useLogin } from 'hooks/useLogin/useLogin'
 import { getData } from 'api/firebase/getSchedules'
+import { getRelativeTime } from 'utils/getRelativeTime'
+import { trackerOptions } from 'config/navbarOptions'
+
+export interface ISchedule {
+  isWorkTime: boolean
+  laps: number
+  maxTime: number
+  methodId: number
+  minutes: number
+  seconds: number
+  step: number
+  time: any
+}
 
 const SchedulePanelPage: React.FC = () => {
-  const [data, setData] = useState([])
+  const [schedules, setSchedules] = useState<ISchedule[]>([])
   const { user } = useLogin()
   const uniqueId = user?.loginHint
 
   useEffect(() => {
-    getData(uniqueId).then((res) => console.log({ res }))
-  }, [])
+    getData(uniqueId as string).then(({ schedules }: any) =>
+      setSchedules(schedules)
+    )
+  }, [uniqueId])
+
+  const handleGroupData = (schedules: ISchedule[]) => {
+    const normalizedSchedule = {} as any
+    schedules.reverse().forEach((schedule) => {
+      const { normalizedDate } = getRelativeTime(schedule.time)
+      if (!normalizedSchedule[normalizedDate]) {
+        normalizedSchedule[normalizedDate] = []
+      }
+      if (normalizedSchedule[normalizedDate]) {
+        normalizedSchedule[normalizedDate].push(schedule)
+      }
+    })
+
+    const arraySchedule = Object.entries(normalizedSchedule)
+
+    console.log(arraySchedule)
+    return arraySchedule
+      .reverse()
+      .map(([timeFormatString, arrayValues]: any) => {
+        let maxTime = 0
+        return (
+          <>
+            <h1
+              key={timeFormatString}
+              className="text-xl mb-2 mt-4 flex items-center"
+            >
+              {timeFormatString}
+              <span className="text-sm bg-gray-100 text-gray-600 mx-2 rounded-md px-2">
+                {arrayValues.length} vueltas
+              </span>
+            </h1>
+            {arrayValues.map((schedule: any, index: number) => {
+              maxTime += schedule.maxTime
+
+              return (
+                <div
+                  key={`${timeFormatString}-${index}`}
+                  className="flex items-center"
+                >
+                  <span
+                    className={`inline-block rounded-full w-3 h-3 mr-2 shadow ${
+                      schedule.isWorkTime
+                        ? 'bg-green-400 shadow-green-200'
+                        : 'bg-orange-400 shadow-orange-200'
+                    }`}
+                  ></span>
+                  <p className="text-gray-600">
+                    {trackerOptions[schedule.methodId].name}
+                  </p>
+                  <span className="mx-2 bg-gray-100 text-gray-600 rounded-md px-2 text-xs">
+                    {schedule.maxTime} min.
+                  </span>
+                </div>
+              )
+            })}
+            <div className="border-t mt-2 py-2">
+              <p className="text-gray-600 text-sm">
+                Tiempo total:{' '}
+                {maxTime > 60
+                  ? (maxTime / 60).toFixed(2) + 'hs.'
+                  : maxTime + 'min.'}
+              </p>
+            </div>
+          </>
+        )
+      })
+  }
 
   return (
     <motion.div className="top-0 right-0 bg-white h-auto lg:h-screen w-80 lg:w-96 shadow-md">
@@ -18,8 +100,10 @@ const SchedulePanelPage: React.FC = () => {
         Panel del Tiempo
       </h1>
       <div className="">
-        <p className="mt-4 text-xl p-2 mb-4">Ve tu progreso</p>
-      
+        <p className="mt-4 text-2xl p-2 mb-4">Ve tu progreso</p>
+      </div>
+      <div className="p-2 -mt-4">
+        {schedules.length > 0 && handleGroupData(schedules)}
       </div>
     </motion.div>
   )
