e134282bc126e3b09d6938d52e0bf810ec3cf446
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-14 16:49:21 -0300

    fix: change "pomodoto" to "FlowTime"

--- src/components/MethodologyHomeTag/PomodoroCustom.tsx
+++ src/components/MethodologyHomeTag/PomodoroCustom.tsx
@@ -1,7 +1,7 @@
 const FlowTimeDescription = () => {
   return (
     <div>
-      Nuestro Pomodoro consiste en una sesión de{' '}
+      Nuestro FlowTime consiste en una sesión de{' '}
       <span className="font-bold text-orange-600">90</span> minutos de trabajo y{' '}
       <span className="font-bold text-green-600">10</span> minutos de descanso.
     </div>
